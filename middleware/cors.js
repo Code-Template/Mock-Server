3dacd4f50deb32d3feaf88b6f2aefc1810a0c5e8
benlv <benlv.hu@dianrong.com>
2017-06-26 13:46:02 +0800

    Dynamic Allow Origin Header

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,9 +1,11 @@
 var middleware = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Max-Age', 3600);
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if(req.headers.origin){
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Credentials', true);
+      res.header('Access-Control-Max-Age', 3600);
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
 
   // intercept OPTIONS method
     if ('OPTIONS' === req.method) {
